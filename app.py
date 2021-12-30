dd4fde867e0c645247eef7a92a47cacb94f2ebb4
Alexander Lacson <41433185+max-torch@users.noreply.github.com>
2021-12-30 11:07:29 +0800

    Test reading from csv in repo

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
+import pandas as pd
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+df = pd.read_csv('zipcodes.csv')
+print(df.Area.iloc[0])
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
@@ -12,6 +17,7 @@ server = app.server
 
 app.layout = html.Div([
     html.H2('Hello World'),
+    html.H2(df.Area.iloc[0]),
     dcc.Dropdown(
         id='dropdown',
         options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
